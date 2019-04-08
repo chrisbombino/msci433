90cf197266e6ae2bbde518120b1f861160aa1404
chrisbombino <32116781+chrisbombino@users.noreply.github.com>
2019-04-08 14:03:24 -0400

    df_trades accurately tracks ROI. portfolio dict now keeps track of total portfolio value

--- ManualStrategy.py
+++ ManualStrategy.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 data = pd.read_csv('test.csv')
 
@@ -11,8 +12,9 @@ def longBuyAction(portfolio, price, shares, df_trades):
     # TODO: check if portfolio has enough cash
     portfolio['long-stock'] += shares
     portfolio['cash'] -= price * shares
+    portfolio['value'] = portfolio['cash'] + price * (portfolio['long-stock'] - portfolio['short-stock'])
 
-    trade = {'Shares': shares, 'Position': 'Enter Long', 'Return': 0.01, 'Portfolio Value': portfolio['cash']}
+    trade = {'Shares': shares, 'Position': 'Enter Long', 'Return': np.nan, 'Portfolio Value': portfolio['value']}
     df_trades = df_trades.append(trade, ignore_index=True)
     return portfolio, df_trades
 
@@ -20,8 +22,10 @@ def longSellAction(portfolio, price, shares, df_trades):
     # TODO: check if portfolio has enough cash
     portfolio['long-stock'] -= shares
     portfolio['cash'] += price * shares
+    portfolio['value'] = portfolio['cash'] + price * (portfolio['long-stock'] - portfolio['short-stock'])
 
-    trade = {'Shares': shares, 'Position': 'Exit Long', 'Return': 0.01, 'Portfolio Value': portfolio['cash']}
+    ret = (portfolio['value'] / df_trades.iloc[-1, 3]) - 1
+    trade = {'Shares': -shares, 'Position': 'Exit Long', 'Return': ret, 'Portfolio Value': portfolio['value']}
     df_trades = df_trades.append(trade, ignore_index=True)
     return portfolio, df_trades
 
@@ -29,8 +33,10 @@ def shortBuyAction(portfolio, price, shares, df_trades):
     # TODO: check if portfolio has enough cash
     portfolio['short-stock'] -= shares
     portfolio['cash'] -= price * shares
+    portfolio['value'] = portfolio['cash'] + price * (portfolio['long-stock'] - portfolio['short-stock'])
 
-    trade = {'Shares': shares, 'Position': 'Exit Short', 'Return': 0.01, 'Portfolio Value': portfolio['cash']}
+    ret = (portfolio['value'] / df_trades.iloc[-1, 3]) - 1
+    trade = {'Shares': shares, 'Position': 'Exit Short', 'Return': ret, 'Portfolio Value': portfolio['value']}
     df_trades = df_trades.append(trade, ignore_index=True)
     return portfolio, df_trades
 
@@ -38,19 +44,21 @@ def shortSellAction(portfolio, price, shares, df_trades):
     # TODO: check if portfolio should short
     portfolio['short-stock'] += shares
     portfolio['cash'] += price * shares
+    portfolio['value'] = portfolio['cash'] + price * (portfolio['long-stock'] - portfolio['short-stock'])
 
-    trade = {'Shares': shares, 'Position': 'Enter Short', 'Return': 0.01, 'Portfolio Value': portfolio['cash']}
+    trade = {'Shares': -shares, 'Position': 'Enter Short', 'Return': np.nan, 'Portfolio Value': portfolio['value']}
     df_trades = df_trades.append(trade, ignore_index=True)
     return portfolio, df_trades
 
 def simulateTrading(df):
     portfolio = {
         'cash': startingCash,
+        'value': startingCash,
         'long-stock': 0,
         'short-stock': 0
     }
-    df_trades = pd.DataFrame(columns=['Shares', 'Position', 'Return', 'Portfolio Value'])
 
+    df_trades = pd.DataFrame(columns=['Shares', 'Position', 'Return', 'Portfolio Value'])
     # start out not holding any position
     state = ''
 
@@ -60,13 +68,11 @@ def simulateTrading(df):
                 # enter long position (buy stock)
                 portfolio, df_trades = longBuyAction(portfolio, row.Price, numShares, df_trades)
                 state = 'long'
-                print portfolio
                 continue
             if not pd.isna(row.SellSignal):
                 # enter short position (short stock)
                 portfolio, df_trades = shortSellAction(portfolio, row.Price, numShares, df_trades)
                 state = 'short'
-                print portfolio
                 continue
             if not pd.isna(row.ExitSignal):
                 # not possible
@@ -81,13 +87,11 @@ def simulateTrading(df):
                 #  exit long position (sell the stock)
                 portfolio, df_trades = longSellAction(portfolio, row.Price, numShares, df_trades)
                 state = ''
-                print portfolio
                 continue
             if not pd.isna(row.ExitSignal):
                 # exit long position (sell the stock)
                 portfolio, df_trades = longSellAction(portfolio, row.Price, numShares, df_trades)
                 state = ''
-                print portfolio
                 continue
 
         # in a short position
@@ -95,7 +99,6 @@ def simulateTrading(df):
             if not pd.isna(row.BuySignal):
                 # exit short position (buy stock)
                 portfolio, df_trades = shortBuyAction(portfolio, row.Price, numShares, df_trades)
-                print portfolio
                 state = ''
                 continue
             if not pd.isna(row.SellSignal):
@@ -104,11 +107,11 @@ def simulateTrading(df):
             if not pd.isna(row.ExitSignal):
                 # exit short position (buy stock)
                 portfolio, df_trades = shortBuyAction(portfolio, row.Price, numShares, df_trades)
-                print portfolio
                 state = ''
                 continue
-    print '\nFinal Portfolio:\n{}'.format(portfolio)
-    print '\nROI = {}%'.format(((portfolio['cash']/startingCash - 1))*100)
+
+    print 'ROI = {}%'.format(((portfolio['cash']/startingCash - 1))*100)
+    print ''
     print df_trades
 
 simulateTrading(data)
